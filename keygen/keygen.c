4afc16964fad75d4dfc1af99093c2246bb993302
David Liu <admin@iamwb.me>
2018-04-01 15:04:34 +0800

    Update keygen.c

--- keygen/keygen.c
+++ keygen/keygen.c
@@ -1,6 +1,6 @@
 Bingo!
-You got it, yes, i'm keygen for this April Fool's Day.
+You got it, yes, i'm a keygen for this April Fool's Day.
 Just enjoy your holidy and we are sure there is no such kind of keymaker/kg.
 
-leader/ZWT
+Dr.Thril/ZWT
 2o18.4.1
